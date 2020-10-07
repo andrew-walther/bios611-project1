d403c4f79f31edc45be775c6132b6a2905e32beb
andrew-walther <69438120+andrew-walther@users.noreply.github.com>
2020-10-07 18:36:36 -0400

    calculated exam summary stats

---
+++ summary_stats.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(mosaic)
+
+StudentsPerformance <- 
+  read_csv("source_data/StudentsPerformance.csv");
+
+Performance_Male <- StudentsPerformance %>% filter(gender=='male');
+
+Performance_Female <- StudentsPerformance %>% filter(gender=='female');
+
+#summmary statistics for math scores
+favstats(StudentsPerformance$math.score);
+favstats(Performance_Male$math.score);
+favstats(Performance_Female$math.score);
+
+#summary statistics for reading scores
+favstats(StudentsPerformance$reading.score);
+favstats(Performance_Male$reading.score);
+favstats(Performance_Female$reading.score);
+
+#summary statistics for writing scores
+favstats(StudentsPerformance$writing.score);
+favstats(Performance_Male$writing.score);
+favstats(Performance_Female$writing.score)
+

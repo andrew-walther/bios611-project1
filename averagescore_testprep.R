8ff9de970dcc0c7ed55877cf089b9167fab74741
andrew-walther <69438120+andrew-walther@users.noreply.github.com>
2020-10-19 01:30:26 -0400

    edited scripts to save figures for report

--- averagescore_testprep.R
+++ averagescore_testprep.R
@@ -1,8 +1,9 @@
 library(tidyverse);
 library(gridExtra);
 
-StudentsPerformance <- 
-  read_csv("source_data/StudentsPerformance.csv");
+#read in data
+StudentsPerformance <- read.csv("~/bios611-project1/source_data/StudentsPerformance.csv");
+#StudentsPerformance <- read_csv("source_data/StudentsPerformance.csv");
 
 #add average score column
 StudentsPerformance <- StudentsPerformance %>% mutate(avg.score = round(rowMeans(StudentsPerformance[,6:8]),2));
@@ -12,4 +13,6 @@ p <- ggplot(StudentsPerformance, aes(x=avg.score, color=test.preparation.course)
   geom_histogram(fill="white", alpha=0.3, position="identity") +
   labs(title = 'Histogram of average exam score by test prep course',x='Average score from three exams');
 
-ggsave("figures/averagescore_testprep.png",plot=p)
+#save figure
+ggsave("~/bios611-project1/figures/averagescore_testprep.png", width = 5, height = 4, plot=p)
+#ggsave("figures/averagescore_testprep.png",plot=p)

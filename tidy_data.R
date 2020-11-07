5b377206f10f917eabe90d68acdd2b5584958d4d
andrew-walther <69438120+andrew-walther@users.noreply.github.com>
2020-11-07 17:40:26 -0500

    adjusted for shiny app

--- tidy_data.R
+++ tidy_data.R
@@ -3,18 +3,17 @@ library(dplyr);
 
 #read in source data
 StudentsPerformance <- 
-  read_csv("source_data/StudentsPerformance.csv");
+        read_csv("source_data/StudentsPerformance.csv");
 
 #create derived dataframes
 average_scores <- StudentsPerformance %>% mutate(avg.score = round(rowMeans(StudentsPerformance[,6:8]),2))
-  
+
 Performance_Male <- StudentsPerformance %>% filter(gender=='male')
 
 Performance_Female <- StudentsPerformance %>% filter(gender=='female')
 
 #save derived dataframes
-
-write_csv(StudentsPerformance, "derived_data/StudentsPerformance.csv");
-write_csv(average_scores, "derived_data/average_scores.csv");
-write_csv(Performance_Male, "derived_data/Performance_Male.csv");
-write_csv(Performance_Female, "derived_data/Performance_Female.csv");
+write_csv(StudentsPerformance, "derived_data/StudentsPerformance.csv")
+write_csv(average_scores, "derived_data/average_scores.csv")
+write_csv(Performance_Male, "derived_data/Performance_Male.csv")
+write_csv(Performance_Female, "derived_data/Performance_Female.csv")
